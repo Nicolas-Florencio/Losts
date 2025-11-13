992cc890a676d3bd17ad71d4bd87ae1cc1f94632
Nicolas-Florencio <nicholasflorencio17@gmail.com>
2025-11-12 23:29:53 -0300

    Ajustes tela de itens

---
+++ front/src/components/FormStatus/FormStatus.jsx
@@ -0,0 +1,12 @@
+import Button from '../Button/Button';
+import styles from './FormStatus.module.css';
+
+export default function FormStatus() {
+    return (
+        <>
+            <p className={styles.opcao}>
+                Adicionar aba com status do item e dois botoes
+            </p>
+        </>
+    )
+}
--- front/src/components/Input/Input.jsx
+++ front/src/components/Input/Input.jsx
@@ -3,8 +3,10 @@ import styles from './Input.module.css';
 export default function Input({labelName, tipo, placeholder, nome}) {
     return (
         <> 
-            <label className={styles.label} htmlFor={nome}>{labelName}</label>
-            <input className={styles.input} type={tipo} name={nome} placeholder={placeholder} />
+            <div className={styles.container}>
+                <label className={styles.label} htmlFor={nome}>{labelName}</label>
+                <input className={styles.input} type={tipo} name={nome} placeholder={placeholder} />
+            </div>
         </>
     )
 }
